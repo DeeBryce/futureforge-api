55ef85b0c8e6d21840615cb9b2fb10f6fb70cc20
DeeBryce <deeamong@gmail.com>
2026-06-25 17:33:14 +0100

    added fixes for AOI

--- models/Applicant.js
+++ models/Applicant.js
@@ -51,7 +51,7 @@ const applicantSchema = new mongoose.Schema({
     areaOfInterest: {
         type: String,
         required: [true, 'Area of interest is required'],
-        enum: ['Frontend', 'Backend', 'Product Design', 'Product Management', 'Quality Assurance', 'Other'] 
+        enum: ['Frontend', 'Backend', 'ProductDesign', 'ProductManagement', 'Quality Assurance', 'Other'] 
     },
     levelOfExperience: {
         type: String,
